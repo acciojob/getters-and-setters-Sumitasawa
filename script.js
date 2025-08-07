c19657b406fc4d2bc5e3c447c0f4ae9f9df98b7e
Sumitasawa <132041180+Sumitasawa@users.noreply.github.com>
2025-08-07 12:03:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-	get name(){
-		return this.name;
-	}
-	get age() {
-    return this.age;
-   }
-	set age(){
-		return this.age=newage;
-	}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for age
+  get age() {
+    return this._age;
+  }
+
+  // Setter for age (takes exactly one argument)
+  set age(newAge) {
+    this._age = newAge;
+  }
 }
 
 class Student extends Person {
-	study() {
+  study() {
     console.log(`${this.name} is studying`);
   }
 }
 
 class Teacher extends Person {
-
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+  teach() {
+    console.log(`${this.name} is teaching`);
+  }
 }
 
 // Do not change the code below this line
